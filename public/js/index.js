1740a8e39042d35d5d0ce4e7f1c2074c71c395f0
Stuart Yang <smyang10@gmail.com>
2017-03-15 22:15:30 -0700

    Added auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+var scrollToBottom = function () {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('connected to server');
 
@@ -23,6 +40,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -42,6 +60,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 // socket.emit('createMessage', {
